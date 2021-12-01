213ce81c378ab1a9b33bc34be769fa615aef2330
aprashant21 <aprashant21@tbc.edu.np>
2021-12-01 11:05:55 +0545

    hello toast coding challenge done

--- app/src/main/java/com/thebigoceaan/hellotoastchallenge/MainActivity.java
+++ app/src/main/java/com/thebigoceaan/hellotoastchallenge/MainActivity.java
@@ -3,9 +3,9 @@ package com.thebigoceaan.hellotoast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
